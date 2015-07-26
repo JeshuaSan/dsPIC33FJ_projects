c558d8ece368926c5e9fce68d41946754a0c7db8
Jesus Sanchez <jesus.san.sev@gmail.com>
2015-07-26 10:53:58 -0500

    deleted unnecesary project files

--- ActivityRobot.X/nbproject/Package-default.bash
+++ ActivityRobot.X/nbproject/Package-default.bash
@@ -10,9 +10,9 @@ CND_CONF=default
 CND_DISTDIR=dist
 TMPDIR=build/${CND_CONF}/${IMAGE_TYPE}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/Servo.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-OUTPUT_BASENAME=Servo.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
-PACKAGE_TOP_DIR=servo.x/
+OUTPUT_PATH=dist/${CND_CONF}/${IMAGE_TYPE}/ActivityRobot.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+OUTPUT_BASENAME=ActivityRobot.X.${IMAGE_TYPE}.${OUTPUT_SUFFIX}
+PACKAGE_TOP_DIR=activityrobot.x/
 
 # Functions
 function checkReturnCode
@@ -57,15 +57,15 @@ mkdir -p ${TMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory ${TMPDIR}/servo.x/bin
+makeDirectory ${TMPDIR}/activityrobot.x/bin
 copyFileToTmpDir "${OUTPUT_PATH}" "${TMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/package/servo.x.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/package/activityrobot.x.tar
 cd ${TMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/servo.x.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/package/activityrobot.x.tar *
 checkReturnCode
 
 # Cleanup
